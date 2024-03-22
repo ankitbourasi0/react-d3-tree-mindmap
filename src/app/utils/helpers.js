5d3dc2fbe57f7482f6670d77b693d6bf1d6d2d71
Ankit Bourasi <ankitbourasi0@gmail.com>
2024-03-22 17:07:16 +0530

    update ref to the tree

--- src/app/utils/helpers.js
+++ src/app/utils/helpers.js
@@ -3,8 +3,8 @@ import { useCallback, useState } from "react";
 export const useCenteredTree = (defaultTranslate = { x: 0, y: 0 }) => {
    
     const [translate, setTranslate] = useState(defaultTranslate);
-  const [dimensions, setDimensions] = useState();
-
+    const [dimensions, setDimensions] = useState();
+ 
 const   containerRef = useCallback((containerElem) => {
     if (containerElem !== null) {
       const { width, height } = containerElem.getBoundingClientRect();
